6753ec83cca5b80b95347e9363ec8712ee3eb95a
Thayroni-Lima <thayronivflima@gmail.com>
2025-05-12 10:58:53 -0300

    3 OK | Ajuste do 2

--- Lista de Exercícios 01 - JS no BackEnd/2.js
+++ Lista de Exercícios 01 - JS no BackEnd/2.js
@@ -5,38 +5,36 @@ controle if-else.
 */
 
 const readline = require('readline');
-const rl = readline.createInterface({input: process.stdin, output: process.stdout});
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
 
-function verificaIdade(){
+function verificaIdade() {
     rl.question("Digite a idade em anos (0-150)\n", (entrada) => {
         const num = Number(entrada);
 
-        if(isNaN(num)){
+        if (isNaN(num)) {
             console.log("O digitado não é um número!");
-            verificaIdade();
+            return verificaIdade();
         }
-        
+
         const idade = parseInt(num);
 
-        if(num < 0 || num > 150){
+        if (num < 0 || num > 150) {
             console.log(`${idade} anos é uma idade não convencional.\n`);
-            verificaIdade();
-        };
+            return verificaIdade();
+        }
 
-
-        if(0 <= idade &&  idade < 14){
+        if (0 <= idade && idade < 14) {
             console.log(`${idade} anos é a idade de uma **criança**!`);
-        }
-        else if(14 <= idade &&  idade < 19){
+        } else if (14 <= idade && idade < 19) {
             console.log(`${idade} anos é a idade de um **adolescente**!`);
-        }
-        else if(19 <= idade &&  idade < 60){
+        } else if (19 <= idade && idade < 60) {
             console.log(`${idade} anos é a idade de um **adulto**!`);
-        }
-        else if(60 <= idade &&  idade <= 150){
+        } else if (60 <= idade && idade <= 150) {
             console.log(`${idade} anos é a idade de um **idoso**!`);
         }
+        
         rl.close();
     });
-};
+}
+
 verificaIdade();
---
+++ Lista de Exercícios 01 - JS no BackEnd/3.js
@@ -0,0 +1,37 @@
+/*
+3. Implemente um programa que recebe uma nota de 0 a 10 e classifica como
+"Aprovado", "Recuperação", ou "Reprovado" utilizando if-else if.
+*/
+
+const readline = require('readline');
+const rl = readline.createInterface({ input: process.stdin, output: process.stdout });
+
+function verificaNota() {
+    rl.question("Digite a Nota (0-10)\n", (entrada) => {
+        const num = Number(entrada);
+
+        if (isNaN(num)) {
+            console.log("O digitado não é um número!");
+            return verificaNota();
+        }
+
+        const Nota = parseInt(num);
+
+        if (num < 0 || num > 10) {
+            console.log(`${Nota} é uma Nota não convencional.\n`);
+            return verificaNota();
+        }
+
+        if (0 <= Nota && Nota < 4) {
+            console.log(`Nota ${Nota}! Você está **reprovado**`);
+        } else if (4 <= Nota && Nota < 7) {
+            console.log(`Nota ${Nota}! Você está em **recuperação**`);
+        } else if (7 <= Nota && Nota <= 10) {
+            console.log(`Nota ${Nota}! Você está **aprovado**`);
+        }
+
+        rl.close();
+    });
+}
+
+verificaNota();
